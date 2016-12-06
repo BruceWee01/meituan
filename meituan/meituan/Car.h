97d5f4f0b45150214f9e98c0f18e6fa828c4e66c
JDWee <13776604033@163.com>
2016-12-07 00:36:52 +0800

    增加了Car类

---
+++ meituan/meituan/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  meituan
+//
+//  Created by 韦锦栋 on 16/12/7.
+//  Copyright © 2016年 JDWee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ meituan/meituan/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  meituan
+//
+//  Created by 韦锦栋 on 16/12/7.
+//  Copyright © 2016年 JDWee. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
